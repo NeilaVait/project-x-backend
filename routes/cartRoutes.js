a570add8111f77fd761626815b9ca3c823245d1d
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-09 10:03:21 +0300

    route add item to cart start

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -7,4 +7,9 @@ router.get('/api/shop/cart/:userId', (req, res) => {
   res.json(`you want to get cart of user ${req.params.userId}`);
 });
 
+// add item to cart
+router.post('/api/shop/cart/:userId', (req, res) => {
+  res.json('you are about to add item to cart');
+});
+
 module.exports = router;
