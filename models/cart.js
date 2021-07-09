59450e2e6bd3e00706244929d3a695c28ef37b87
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-09 09:47:20 +0300

    cart model

---
+++ models/cart.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const reqString = {
+  type: String,
+  required: true,
+};
+const reqNumber = {
+  type: Number,
+  required: true,
+};
+
+const cartSchema = new Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    cart: [
+      {
+        price: reqNumber,
+        color: reqString,
+        size: reqString,
+        sku: reqString,
+        itemId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: 'shopItem',
+          required: true,
+        },
+        quantity: reqNumber,
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+const Cart = mongoose.model('cart', cartSchema);
+
+module.exports = Cart;
