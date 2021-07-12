715e01d4b83c08ade1a110b0faddf2ddc62d87b0
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-12 11:21:43 +0300

    quantity++ on same item to cart

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -4,45 +4,44 @@ const router = express.Router();
 
 // get user cart
 router.get('/api/shop/cart/:userId', (req, res) => {
-  res.json(`you want to get cart of user ${req.params.userId}`);
+  res.json(`You want to get cart of a user ${req.params.userId}`);
 });
 
 // add item to cart
 router.post('/api/shop/cart/:userId', async (req, res) => {
   console.log('got item to add to cart');
-  console.log(req.body);
+  // console.log(req.body);
 
   try {
-    // ar toks krepselis egzistuoja
+    // ar toks krepselis existuoja
     const currentCart = await Cart.findOne({ userId: req.params.userId }).exec();
-    console.log('currentCart', Boolean(currentCart));
-
-    // jei nera tokio cart kuriam nauja
+    console.log(' currentCart', Boolean(currentCart));
+    // jei jau yra toks cart tai mes norim prideti prie cart objektu
     if (!currentCart) {
       const newCart = new Cart({ userId: req.params.userId, cart: [req.body] });
       const result = await newCart.save();
-      res.json({ msg: 'created cart', result });
+      res.json({ msg: 'created a cart', result });
     } else {
-      // currentCart nelygu nuliui/ cartas jau egzistuoja
+      // count nelygu nuliui cartas siam vartotojui egzistuoja norim prideti i cart
+      //currentCartArr esamas krepselis db
+      // req.body = naujas item i krepseli
       const currentCartArr = currentCart.cart;
-
-      // ar toks daiktas ir dydis jau yra pas mus masyve
-      const isItemInCartAlready = currentCartArr.find((i) => i.itemId == req.body.itemId);
-      console.log('isItemInCartAlready', isItemInCartAlready);
-      // req.body.size yra currentCartArr
-      if (req.body.size === isItemInCartAlready.size) {
+      const isItemInCartAlready = currentCartArr.find(
+        (ci) => ci.itemId == req.body.itemId && ci.size === req.body.size
+      );
+      if (isItemInCartAlready) {
+        // qty ++
         isItemInCartAlready.quantity++;
-        console.log('arr atsinaujino', currentCartArr);
+        console.log('qty++');
       } else {
+        console.log('item push');
         currentCartArr.push(req.body);
       }
-
       await Cart.updateOne({ userId: req.params.userId }, { cart: currentCartArr });
-
-      res.json({ msg: 'now in cart', currentCart: currentCart.cart });
+      res.json({ msg: 'now in cart', currentCart });
     }
 
-    // res.json('testing')
+    // res.json('testing');
   } catch (err) {
     res.json(err);
   }
