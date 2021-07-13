51787bd0ee687ef0d6869aaf03af25ab02cbcbcf
NeilaVait <79705047+NeilaVait@users.noreply.github.com>
2021-07-13 09:24:19 +0300

    try catch in count

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -6,13 +6,17 @@ const router = express.Router();
 // get count of carts of a user
 router.get('/api/shop/cart/count/:userId', async (req, res) => {
   // gauti ta karta kurios userId yra lygus parametruose paduotam :userId
-  const currentUserCartObj = await Cart.findOne({ userId: req.params.userId }).exec();
-  console.log('currentUserCartObj', currentUserCartObj);
-  if (currentUserCartObj && currentUserCartObj.cart) {
-    // grazinti co masyvo ilgi
-    return res.json(currentUserCartObj.cart.length);
+  try {
+    const currentUserCartObj = await Cart.findOne({ userId: req.params.userId }).exec();
+    console.log('currentUserCartObj', currentUserCartObj);
+    if (currentUserCartObj && currentUserCartObj.cart) {
+      // grazinti co masyvo ilgi
+      return res.json(currentUserCartObj.cart.length);
+    }
+    res.status(200).json(0);
+  } catch (err) {
+    res.json(err);
   }
-  res.status(200).json(0);
 });
 
 // get user cart
@@ -32,7 +36,6 @@ router.get('/api/shop/cart/:userId', async (req, res) => {
   }
 });
 
-// add item to cart
 // add item to cart
 router.post('/api/shop/cart/:userId', async (req, res) => {
   console.log('got item to add to cart');
